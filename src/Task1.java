874d6dfd5bf42662a3addd7c03f46e47f6f3cd0e
albnsp <a.suprunova1@gmail.com>
2023-11-08 01:04:48 +0400

    commit add Task-1.java

--- src/Task1.java
+++ src/Task1.java
@@ -5,5 +5,6 @@ public class Task1 {
         int mask = ~(1 << 0); // маска для обнуления бита в нулевом разряде
         int result = N & mask; // обнуление бита в нулевом разряде
         System.out.println(Integer.toBinaryString(result));
+
     }
 }
